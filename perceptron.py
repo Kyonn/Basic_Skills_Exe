1d059bb2a141aaa50f36637d4877e3ef1c27db7e
Riku Sato <rikusato@MacMini-rsato.local>
2019-05-21 15:23:13 +0900

    収束判定未実装

--- perceptron.py
+++ perceptron.py
@@ -1,23 +1,34 @@
 import numpy as np
 import csv
 
-
+#識別関数
 def identify(w_vec, x_vec):
-    y = np.dot(w_vec, x_vec): 
+    y = np.dot(w_vec, x_vec) 
     if y >= 0:
         y = 1
     else:
         y = -1
     return y
 
-def update(w_vec, x_vec, rho):
-    if identify(w_vec, x_vec) < 0:
+#重みの更新
+def update(w_vec, x_vec, rho, t): #rho:学習係数(0 ~ 1), t:ラベル(1 or -1)
+    if identify(w_vec, x_vec) != t:
         w_vec = w_vec + rho * t * x_vec
-        return w_vec
-    else:
-        return w_vec
+    return w_vec
 
 if __name__ ==  "__main__":
-    weight = np.array([0,0,0])
+    weight = np.array([-1,1,1])
+    rho = 0.5
+
+    while abs(weight - weight_new < 0.001):
+        weight = weight_new
+        weight_new = update(weight, x_train[0:3], rho, x_train[3])
+        
+
+
+
     
+    
+
+
 
